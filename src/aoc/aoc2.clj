b86d67ba853dedb5f53915f288cc1e7c6c0f0184
Josh Reighley <josh@reighley.net>
2019-12-02 02:05:34 -0800

    Added Day 2 docstrings and tidied code

--- src/aoc/aoc2.clj
+++ src/aoc/aoc2.clj
@@ -1,12 +1,16 @@
 (ns aoc.aoc2
   (:require [clojure.string :as s]))
 
+;; read data file
 (def data (doall (mapv #(Integer/parseInt %)(-> (slurp "resources/input2")
                                               (s/split #"\n")
                                               (first)
                                               (s/split #",")))))
+;;fn to do return nothing if op end code
 (defn op-end [_ _]
   nil)
+
+;;translate ints to apporpiate fns
 (def commands {1 +
                2 *
                99 op-end})
@@ -23,37 +27,50 @@
      (if (= 99 (first more))
        (assoc command-seq output calc-result)
        (recur (assoc command-seq output calc-result) (+ 4 cursor-pos))))))
+;; not sure this is safe -- but it works for my data.
+;; But if the 99 is mod 4 and last I suspect trouble
 
 (def repaired-data
   (-> data
     (assoc 1 12)
     (assoc 2 2)))
 
+(def answer1 (first (process-ops repaired-data)))
+
 (def nouns (range 0 100))
 (def verbs (range 0 100))
 
-(defn configure-data [noun verb]
+(defn insert-noun-verb [noun verb]
   (-> data
       (assoc 1 noun)
       (assoc 2 verb)))
 
-(defn all-anwers []
+(defn all-anwers
+  "calculates and maps responses for every noun verb combination"
+  []
   (let [iterations (for [noun nouns
                          verb verbs]
                      [noun verb])
         results (for [[noun verb] iterations]
                   {:noun noun
                    :verb verb
-                   :result (->> (configure-data noun verb)
+                   :result (->> (insert-noun-verb noun verb)
                                 (process-ops)
                                 (first))})]
     results))
 
-(def correct-result (first (filter #(= 19690720 (:result %)) (all-anwers))))
+(defn simplify-answer [correct-result]
+  (+ (* 100 (:noun correct-result)) (:verb correct-result)))
 
-(def answer2 (+ (* 100 (:noun correct-result)) (:verb correct-result)))
+(defn  reverse-engineer
+  "filter all possible responses to the onewe care about"
+  [result]
+  (first (filter #(= result (:result %)) (all-anwers))))
 
-(def answer1 (first (process-ops repaired-data)))
+(def answer2
+  (->> 19690720
+    (reverse-engineer)
+    (simplify-answer)))
 
 
 
